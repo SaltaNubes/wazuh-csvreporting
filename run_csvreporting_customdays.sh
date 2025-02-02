9661b4068b825cf10950eefca4b328319dbda893
SaltaNubes <55571104+SaltaNubes@users.noreply.github.com>
2025-02-02 00:42:06 -0600

    Update run_csvreporting_customdays.sh
    Añadido opcion de seleccionar intervalos de tiempo

--- run_csvreporting_customdays.sh
+++ run_csvreporting_customdays.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Función para generar las fechas en el rango especificado
+# Función para generar las fechas y horas en el rango especificado
 generate_dates() {
     start_date=$1
     end_date=$2
+    interval=$3
     dates=()
     current_date=$start_date
 
@@ -13,8 +14,8 @@ generate_dates() {
 
     while [ "$start_date_epoch" -le "$end_date_epoch" ]; do
         dates+=("$current_date")
-        # Incrementar un día
-        current_date=$(date -I -d "$current_date + 1 day")
+        # Incrementar el intervalo especificado (en horas)
+        current_date=$(date -Iseconds -d "$current_date + $interval hours")
         start_date_epoch=$(date -d "$current_date" +%s)
     done
 
@@ -33,6 +34,7 @@ fi
 
 # Solicitar el valor de --config al usuario
 read -p "Introduce el valor de --config: " config_value
+config_value="${config_value}"
 
 # Validar que no esté vacío
 if [ -z "$config_value" ]; then
@@ -40,15 +42,36 @@ if [ -z "$config_value" ]; then
     exit 1
 fi
 
-# Generar el rango de fechas
-dates=$(generate_dates "$start_date" "$end_date")
+# Solicitar el intervalo al usuario
+read -p "Introduce el intervalo en horas (4, 8, 12, 24) [24]: " interval
+interval=${interval:-24}
+
+# Validar que el intervalo sea uno de los valores permitidos
+if ! [[ "$interval" =~ ^(4|8|12|24)$ ]]; then
+    echo "Error: El intervalo debe ser uno de los siguientes valores: 4, 8, 12, 24"
+    exit 1
+fi
+
+# Generar el rango de fechas con el intervalo especificado
+dates=$(generate_dates "${start_date}T00:00:00-0600" "${end_date}T23:59:59-0600" $interval)
 
 # Preparar las líneas de comando
 commands=()
 for date in $dates; do
-    desde="${date}T00:00:00.000-0600"
-    hasta="${date}T23:59:59.999-0600"
-    cmd="python3 csvreporting-customdays.py --config $config_value --desde $desde --hasta $hasta --debug"
+    desde=$date
+    hasta=$(date -Iseconds -d "$date + $interval hours")
+
+    # Ajustar la hora de finalización al final del día, si es necesario
+    date_day=$(date -d "$date" +%Y-%m-%d)
+    hasta_day=$(date -d "$hasta" +%Y-%m-%d)
+    if [[ "$date_day" != "$hasta_day" ]]; then
+        hasta="${date_day}T23:59:59-0600"
+    else
+        # Restar un segundo para que el hasta sea HH:59:59 en lugar de HH:00:00
+        hasta=$(date -Iseconds -d "$hasta - 1 second")
+    fi
+
+    cmd="python3 csvreporting-customdays-scrollapi.py --config $config_value --desde $desde --hasta $hasta --debug"
     commands+=("$cmd")
 done
 
